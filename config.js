0225145e03f0fc70798fed82f090f52814d60246
root <root@chat64.ejabberddev.localdomain>
2013-10-28 17:29:10 -0400

    compile error and add more configs to fil

--- config.js
+++ config.js
@@ -2,46 +2,68 @@
 (function (environment) {
   var fs = {};
   var Env = process.env.Environment || 'undefined';
-  var ejabConfig = { 
-	host: '';
-   	port: ''; };
-
-  var sparkApi: {
-        spark_api_endpoint: '',
-        spark_app_id: '',
-        spark_brand_id: '',
-        spark_client_secret: '',
-        spark_create_oauth_accesstoken: '/brandId/{brandId}/oauth2/accesstoken/application/{applicationId}',
-        auth_profile_miniProfile: '/brandId/{brandId}/profile/attributeset/miniProfile/{targetMemberId}',
-        profile_memberstatus: '/brandId/{brandId}/application/{applicationId}/member/{memberId}/status'
-    },
-   var community2brandId: [
-        {
-            name: 'spark',
-            communityId: '1',
-            brandId: '1001'
-        },
-        {
-            name: 'jdate',
-            communityId: '3',
-            brandId: '1003'
-        },
-        {
-            name: 'cupid',
-            communityId: '10',
-            brandId: '1015'
-        },
-        {
-            name: 'bbw',
-            communityId: '23',
-            brandId: '90410'
-        },
-        {
-            name: 'blacksingle',
-            communityId: '24',
-            brandId: '90510'
-        }
-    ]  
+  var ejabConfig = 
+	{ "ejabConfig": [  
+	  {host: 'undefined'},
+	  {port: 'undefined'}
+	  ]
+	};
+  
+  var log4jConfig = 
+	{   "appenders": [
+      	     {
+          	 type: "console",
+                 category: "console"
+             },
+             {
+          	 "type": "file",
+         	 "filename": "tmp-test.log",
+          	 "maxLogSize": 1024,
+            	 "backups": 3,
+          	 "category": "test"
+      	     }]
+  	};
+ 
+  var sparkApi = 
+	{ "sparkApi": [ 
+	     {spark_api_endpoint: 'undefined'},
+             {spark_app_id: 'undefined'},
+             {spark_brand_id: 'undefined'},
+             {spark_client_secret: 'undefined'},
+             {spark_create_oauth_accesstoken: '/brandId/{brandId}/oauth2/accesstoken/application/{applicationId}'},
+             {auth_profile_miniProfile: '/brandId/{brandId}/profile/attributeset/miniProfile/{targetMemberId}'},
+             {profile_memberstatus: '/brandId/{brandId}/application/{applicationId}/member/{memberId}/status'}
+	  ]
+	};
+   var community2brandId =
+	{"communityBrandIdMap": [
+	        {
+	            name: 'spark',
+	            communityId: '1',
+	            brandId: '1001'
+	        },
+	        {
+	            name: 'jdate',
+	            communityId: '3',
+	            brandId: '1003'
+	        },
+	        {
+	            name: 'cupid',
+	            communityId: '10',
+	            brandId: '1015'
+	        },
+	        {
+	            name: 'bbw',
+	            communityId: '23',
+	            brandId: '90410'
+	        },
+	        {
+	            name: 'blacksingle',
+	            communityId: '24',
+	            brandId: '90510'
+	        }
+	   ]
+	};  
 
   try {
      fs = require('graceful-fs');
@@ -56,50 +78,53 @@
     switch(Env)
     {
 	case 'dev':
-        	sparkApi.spark_api_endpoint = 'http://api.stgv3.spark.net/v2',
-        	sparkApi.spark_app_id =  '1054',
-        	sparkApi.spark_brand_id = '90510',
-        	sparkApi.spark_client_secret = 'nZGVVfj8dfaBPKsx_dmcRXQml8o5N-iivf5lBkrAmLQ1'		
+		ejabConfig.host = 'chat64.ejabberddev.localdomain';
+		ejabConfig.port = 5280;  
+        	sparkApi.spark_api_endpoint = 'http://api.stgv3.spark.net/v2';
+        	sparkApi.spark_app_id =  '1054';
+        	sparkApi.spark_brand_id = '90510';
+        	sparkApi.spark_client_secret = 'nZGVVfj8dfaBPKsx_dmcRXQml8o5N-iivf5lBkrAmLQ1';		
 		break;
 	case 'stage':
-        	sparkApi.spark_api_endpoint = 'http://api.stgv3.spark.net/v2',
-        	sparkApi.spark_app_id = '1054',
-        	sparkApi.spark_brand_id = '90510',
-        	sparkApi.spark_client_secret = 'nZGVVfj8dfaBPKsx_dmcRXQml8o5N-iivf5lBkrAmLQ1'			
-		
+		ejabConfig.host = 'chat.stgv3.spark.net';
+		ejabConfig.port = 5280;  
+        	sparkApi.spark_api_endpoint = 'http://api.stgv3.spark.net/v2';
+        	sparkApi.spark_app_id = '1054';
+        	sparkApi.spark_brand_id = '90510';
+        	sparkApi.spark_client_secret = 'nZGVVfj8dfaBPKsx_dmcRXQml8o5N-iivf5lBkrAmLQ1';			
+		break;
 	case 'preprod':
-        	sparkApi.spark_api_endpoint = 'http://api.stgv3.spark.net/v2',
-        	sparkApi.spark_app_id = '1054',
-        	sparkApi.spark_brand_id = '90510',
-        	sparkApi.spark_client_secret = 'nZGVVfj8dfaBPKsx_dmcRXQml8o5N-iivf5lBkrAmLQ1'	
-
+		ejabConfig.host = 'chat.stgv3.spark.net';
+		ejabConfig.port = 5280;  
+        	sparkApi.spark_api_endpoint = 'http://api.stgv3.spark.net/v2';
+        	sparkApi.spark_app_id = '1054';
+        	sparkApi.spark_brand_id = '90510';
+        	sparkApi.spark_client_secret = 'nZGVVfj8dfaBPKsx_dmcRXQml8o5N-iivf5lBkrAmLQ1';	
+		break;
 	case 'prod':
-        	sparkApi.spark_api_endpoint = 'undefined',
-        	sparkApi.spark_app_id = 'undefined',
-        	sparkApi.spark_brand_id = 'undefined',
-        	sparkApi.spark_client_secret = 'undefined',
+		ejabConfig.host = 'chat.stgv3.spark.net';
+		ejabConfig.port = 5280;  
+        	sparkApi.spark_api_endpoint = 'undefined';
+        	sparkApi.spark_app_id = 'undefined';
+        	sparkApi.spark_brand_id = 'undefined';
+        	sparkApi.spark_client_secret = 'undefined';
+		break;
 
 	default:
-        	sparkApi.spark_api_endpoint = 'undefined',
-        	sparkApi.spark_app_id = 'undefined',
-        	sparkApi.spark_brand_id = 'undefined',
-        	sparkApi.spark_client_secret = 'undefined',
 		throw new 'Unknown environment value!!!!!!!!!!!!!!!!';
-
     };     
-    console.log('Api Environment '+ );
-    console.log('Api Endpoint' + sparkApi.spark_api_endpoint = 'undefined',
-    console.log('' + sparkApi.spark_app_id = 'undefined',
-    console.log('' + sparkApi.spark_brand_id = 'undefined',
-    console.log('' +sparkApi.spark_client_secret = 'undefined',
+    console.log(Env + ': Ejabberd Host: ' + ejabConfig.host);
+    console.log(Env + ': Ejabberd Port: ' + ejabConfig.port);
+    console.log(Env + ': Api Endpoint: ' + sparkApi.spark_api_endpoint);
+    console.log(Env + ': AppId: ' + sparkApi.spark_app_id);
+    console.log(Env + ': BrandId: ' + sparkApi.spark_brand_id);
+    console.log(Env + ': Client Secret:' +sparkApi.spark_client_secret);
   } catch (error) {
      var errorMsg = 'Error reading config. Environment: ' + process.env.Environment;
      console.log(errorMsg);
      throw new Error(errorMsg)
   };
-  var Host = 
-  var Port = 
-  var Constructor = (function(configVal) {
+  var Constructor = (function() {
 	this.ejab_config = (function(error, configJson) {
     		if(error) {
       			console.log('Error reading config %d', error);		
@@ -118,8 +143,8 @@
 		};
 		log4jConf = configJson.get('log4j');
 		log4js.configure(JSON.stringify(log4jConf))
-                })(configVal);	
-  return new Constructor(config);})(config);
+                })();	
+  return new Constructor();})();
 })();
 
 

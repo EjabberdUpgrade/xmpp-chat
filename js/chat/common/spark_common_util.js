fd42f7bef8a7e4d2ecf2f99ca627304bf3f8d92a
root <root@chat64.ejabberddev.localdomain>
2013-11-22 18:07:18 -0500

    add deprecate function

---
+++ js/chat/common/spark_common_util.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function warn_deprecate(callback, name, suggestion) {
+  return function() {
+    if( typeof console !== 'undefined' && 
+	typeof console.warn === 'function') {
+            console.warn(name + " is deprecated, use " + suggestion + "instead.", new Error("").stack ) 
+	}
+    return callback.apply(this, arguments);
+    };
+};
